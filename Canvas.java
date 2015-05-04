e24ef44133b8abd7b521d37ead68c7c258384be7
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2015-05-04 20:12:59 +0200

    center image

--- Canvas.java
+++ Canvas.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.geom.*;
 import java.awt.image.*;
 import java.io.*;
 import java.util.*;
@@ -12,6 +13,7 @@ final class Canvas extends JComponent {
 	static String[] extensions = new String[] {".jpeg", ".jpg"}; // extensions recognized by scan
 	File[] photos = new File[0];                                 // scanned photos from dir
 	BufferedImage image;                                         // current photo
+	AffineTransform transf = new AffineTransform();              // transform on current image
 	RenderingHints hints;                                        // quality settings
 
 	// New Canvas wit directory to scan
@@ -48,10 +50,30 @@ final class Canvas extends JComponent {
 
 	void loadImg(int i) throws IOException {
 		if(photos.length == 0) {
-			this.image = null;
+			image = null;
 			return;
 		}
 		this.image = ImageIO.read(photos[i]);
+	}
+
+	void zoomFit() {
+		if (image == null) {
+			return;
+		}
+
+		double w = (double)(getWidth());          // canvas size
+		double h = (double)(getHeight());
+		double imw = (double)(image.getWidth());  // image size
+		double imh = (double)(image.getHeight());
+
+		double zx = w / imw;                      // zoom to fit
+		double zy =  h /imh;
+		double zoom = (zx < zy? zx: zy);
+		transf.setToScale(zoom, zoom);
+
+		double tx = (w/zoom - imw)/2;             // translate to center
+		double ty = (h/zoom - imh)/2;
+		transf.translate(tx, ty);
 	}
 
 	public void paintComponent(Graphics g_) {
@@ -60,11 +82,10 @@ final class Canvas extends JComponent {
 		Graphics2D g = (Graphics2D)(g_);
 		g.setRenderingHints(hints);
 
+		zoomFit();
 
 		if (this.image != null) {
-			int dx = (getWidth() - image.getWidth())/2;
-			int dy = (getHeight() - image.getHeight())/2;
-			g.drawImage(this.image, null, dx, dy);
+			g.drawImage(this.image, transf, null);
 		}
 
 	}
